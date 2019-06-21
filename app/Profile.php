aa51e9af500a12dd533ef4bb5dfdce793264e789
Etje <etien_ruhl@live.nl>
2019-06-22 00:16:50 +0200

    initial commit. Changes made to 3 hours and 37 minutes

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $guarded = [];
+
+    public function profileImage(){
+        $imagePath = ($this->image) ? $this->image : '/uploads/P47GRN9H7MXVzr4t31tD7Fqd9QzURQXK63r4j3IM.png';
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers() {
+        return $this->belongsToMany(User::class);
+    }
+
     public function user() {
         return $this->belongsTo(user::class);
     }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,15 +4,27 @@
 <div class="container">
 <div class="row">
     <div class="col-3">
-        <img src="/svg/freeRandomLogo.svg" class="rounded-circle w-50 d-block m-5">
+        <img src="{{ $user->profile->profileImage() }}" class="rounded-circle w-50 d-block m-5">
     </div>
     <div class="col-9 pt-5 ">
         <div class="d-flex justify-content-between align-items-baseline">
-            <h1>{{ $user->username }}</h1>
-            <a href="#">Add new post</a>
+            <div class="d-flex align-items-center pb-3">
+                <div class="h4">{{ $user->username }}</div>
+
+                <follow-button user-id="{{ $user->id }}"></follow-button>
+            </div>
+            @can('update', $user->profile)
+                <a href="/p/create">Add new post</a>
+            @endcan
+            
         </div>
+
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
+
         <div class="d-flex">
-            <div class="pr-5"><strong>153</strong> posts</div>
+            <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
             <div class="pr-5"><strong>24k</strong> followers</div>
             <div class="pr-5"><strong>212</strong> following</div>
         </div>
@@ -22,15 +34,15 @@
     </div>
 </div>
 <div class="row pt-5">
-    <div class="col-4">
-        <img src="https://scontent-ams4-1.cdninstagram.com/vp/967ae60b4e32ce9935b90ae0da0d69be/5D86FBBE/t51.2885-15/sh0.08/e35/s640x640/37181091_286466225443110_1522114161771806720_n.jpg?_nc_ht=scontent-ams4-1.cdninstagram.com" alt="" class="w-100 d-block mx-auto">
+
+    @foreach($user->posts as $post)
+    <div class="col-4 pb-4">
+        <a href="/p/{{ $post->id }}">
+            <img src="/storage/{{ $post->image }}" alt="" class="w-100 d-block mx-auto">
+        </a>
     </div>
-    <div class="col-4">
-        <img src="https://scontent-ams4-1.cdninstagram.com/vp/f0928735457b4dd88ef649ccc7b21743/5DBFC7DE/t51.2885-15/sh0.08/e35/c99.0.882.882a/s640x640/43028259_512289225937513_7531442458971474899_n.jpg?_nc_ht=scontent-ams4-1.cdninstagram.com" alt="" class="w-100 d-block mx-auto">
-    </div>
-    <div class="col-4">
-        <img src="https://scontent-ams4-1.cdninstagram.com/vp/de08f3b51f2eba9b71d8aac69bbb4deb/5D8B44D5/t51.2885-15/sh0.08/e35/s640x640/13735834_1721657601428916_1169530594_n.jpg?_nc_ht=scontent-ams4-1.cdninstagram.com" alt="" class="w-100 d-block mx-auto">
-    </div>
+    @endforeach
+
 </div>
 </div>
 @endsection
